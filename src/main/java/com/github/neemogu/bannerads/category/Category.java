4278a7cfc51df4b158732900592354fd2667fb30
Nikita <nik180101@gmail.com>
2021-08-30 15:49:43 +0700

    category entity

---
+++ src/main/java/com/github/neemogu/bannerads/category/Category.java
@@ -0,0 +1,31 @@
+package com.github.neemogu.bannerads.category;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    @NotNull(message = "Category name cannot be null")
+    @Size(min = 1, max = 255, message = "Category name must be from 1 to 255 symbols length")
+    private String name;
+    @Column(name = "req_name")
+    @NotNull(message = "Category request name cannot be null")
+    @Size(min = 1, max = 255, message = "Category request name must be from 1 to 255 symbols length")
+    private String reqName;
+    @Column(name = "deleted")
+    @JsonIgnore
+    private Boolean deleted;
+}
