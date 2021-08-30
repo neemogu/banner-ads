b183c1cbf3da855c841adc5434b2b0206daff862
Nikita <nik180101@gmail.com>
2021-08-30 16:34:36 +0700

    request entity

---
+++ src/main/java/com/github/neemogu/bannerads/request/Request.java
@@ -0,0 +1,30 @@
+package com.github.neemogu.bannerads.request;
+
+import com.github.neemogu.bannerads.banner.Banner;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "request")
+public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne(targetEntity = Banner.class)
+    @JoinColumn(name = "banner_id")
+    private Banner banner;
+
+    @Column(name = "user_agent")
+    private String userAgent;
+    @Column(name = "ip_address")
+    private String ipAddress;
+    @Column(name = "date")
+    private Date date;
+}
