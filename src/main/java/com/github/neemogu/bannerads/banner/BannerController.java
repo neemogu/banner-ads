da62e93a3d4912766b10ddf7f2234ef448cb4b24
Nikita <nik180101@gmail.com>
2021-08-31 23:42:06 +0700

    banner controller

---
+++ src/main/java/com/github/neemogu/bannerads/banner/BannerController.java
@@ -0,0 +1,61 @@
+package com.github.neemogu.bannerads.banner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/banners")
+public class BannerController {
+    private final BannerService service;
+
+    @Autowired
+    public BannerController(BannerService service) {
+        this.service = service;
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<String> addBanner(@RequestBody @Valid Banner banner) {
+        banner.setId(null);
+        Optional<String> error = service.saveBanner(banner);
+        return error
+                .map(s -> new ResponseEntity<>(s, HttpStatus.BAD_REQUEST))
+                .orElseGet(() -> ResponseEntity.ok("OK"));
+    }
+
+    @PutMapping(consumes = "application/json")
+    public ResponseEntity<String> updateBanner(@RequestBody @Valid Banner banner) {
+        Optional<String> error = service.saveBanner(banner);
+        return error
+                .map(s -> new ResponseEntity<>(s, HttpStatus.BAD_REQUEST))
+                .orElseGet(() -> ResponseEntity.ok("OK"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBanner(
+            @PathVariable("id") Integer id
+    ) {
+        service.deleteBanner(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Banner>> getBannersList(
+            @RequestParam(name = "searchName", defaultValue = "") String searchName
+    ) {
+        return ResponseEntity.ok(service.getBannerList(searchName));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Banner> getSpecificBanner(
+            @PathVariable Integer id
+    ) {
+        Optional<Banner> result = service.getSpecificBanner(id);
+        return result.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
